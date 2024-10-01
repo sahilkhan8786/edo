c72c37d060d7ff59fe7a82a669219d111d6a3bf5
sahilkhan8786 <official.khansahil8786@gmail.com>
2024-10-01 09:29:28 +0530

    updated the UI

--- src/components/Carousal.jsx
+++ src/components/Carousal.jsx
@@ -24,12 +24,70 @@ const HorizontalScrollCarousel = () => {
     });
 
     // Adjust scrolling logic to ensure all images scroll
-    const x = useTransform(scrollYProgress, [0, 1], ["100%", windowWidth >= 768 ? '-0%' : '-80%']);
-    const scaleLast = useTransform(scrollYProgress, [0.8, 1], [1, 2]);
-    const translateLast = useTransform(scrollYProgress, [0.8, 1], [0, 0]); // Keep it centered
+    const x = useTransform(
+        scrollYProgress,
+        [0, 1],
+        [
+            "100%",
+            windowWidth >= 4200
+                ? '85%' :
+                windowWidth >= 3900
+                    ? '75%' :
+                    windowWidth >= 3600
+                        ? '65%' :
+                        windowWidth >= 3300
+                            ? '55%' :
+                            windowWidth >= 3000
+                                ? '45%' :
+                                windowWidth >= 2800
+                                    ? '35%' :
+                                    windowWidth >= 2600
+                                        ? '25%' :
+                                        windowWidth >= 2300
+                                            ? '15%' :
+                                            windowWidth >= 2100
+                                                ? '-0%' :
+                                                windowWidth >= 2000
+                                                    ? '-5%' :
+                                                    windowWidth >= 1900
+                                                        ? '-10%' :
+                                                        windowWidth >= 1700
+                                                            ? '-20%' :
+                                                            windowWidth >= 1500
+                                                                ? '-28%' :
+                                                                windowWidth >= 1400
+                                                                    ? '-30%' :
+                                                                    windowWidth >= 1300
+                                                                        ? '-35%' :
+                                                                        windowWidth >= 1200
+                                                                            ? '-40%' :
+                                                                            windowWidth >= 1100
+                                                                                ? '-45%' :
+                                                                                windowWidth >= 1024
+                                                                                    ? '-52%' :
+                                                                                    windowWidth >= 900
+                                                                                        ? '-55%'
+                                                                                        : windowWidth >= 768
+                                                                                            ? '-60%'
+                                                                                            : windowWidth >= 700
+                                                                                                ? '-65%' :
+                                                                                                windowWidth >= 600
+                                                                                                    ? '-70%' :
+                                                                                                    windowWidth >= 500
+                                                                                                        ? '-75%'
+                                                                                                        : windowWidth >= 425
+                                                                                                            ? '-79%'
+                                                                                                            : '-82%'
+        ]
+    );
+    const scaleLast = useTransform(scrollYProgress, [0.8, 1], [1, 1.2]);
 
     return (
-        <section ref={targetRef} className="relative h-[150vh]">
+        <section ref={targetRef} className="relative h-[250vh]">
+            <div className="w-full h-screen flex items-center justify-center">
+
+                <h1 className="text-center size-16 bg-primary-green rounded-full flex items-center justify-center text-white text-2xl animate-bounce cursor-pointer hover:bg-transparent hover:border hover:border-primary-green hover:text-primary-green">˅</h1>
+            </div>
             <div className="sticky top-0 flex h-screen items-center overflow-hidden">
                 <motion.div style={{ x }} className="flex gap-3">
                     {cards.map((card, index) => (
@@ -56,16 +114,19 @@ const Card = ({ card, isLast, scaleLast, scrollYProgress }) => {
     const opacity = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
 
     return (
-        <motion.div
-            className="group relative w-[70vw] max-w-[250px] h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] overflow-hidden"
-            style={{ ...positionStyles, opacity }} // Apply fade-in effect
-        >
-            <img
-                src={card.url}
-                alt={card.title}
-                className="absolute inset-0 w-full h-full object-contain transition-transform duration-300 group-hover:scale-110"
-            />
-        </motion.div>
+        <>
+
+            <motion.div
+                className="group relative w-[70vw] max-w-[250px] h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] overflow-hidden"
+                style={{ ...positionStyles, opacity }} // Apply fade-in effect
+            >
+                <img
+                    src={card.url}
+                    alt={card.title}
+                    className="absolute inset-0 w-full h-full object-contain transition-transform duration-300 "
+                />
+            </motion.div>
+        </>
     );
 };
 
@@ -93,3 +154,6 @@ const cards = [
         id: 4,
     },
 ];
+
+
+
